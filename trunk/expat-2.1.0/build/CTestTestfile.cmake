c0b855c06c3a8605a666c8e6a670dcfb1069f86e
Cihan SARI <cihansari86@gmail.com>
2014-09-29 20:34:49 +0000

    New toolchain

--- trunk/expat-2.1.0/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Projects/expat-2.1.0
-# Build directory: D:/Projects/expat-2.1.0-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-ADD_TEST(runtests "runtests")
-ADD_TEST(runtestspp "runtestspp")
